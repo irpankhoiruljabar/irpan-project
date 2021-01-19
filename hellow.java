1d4b9e37c02145ef0795467433b70637e87bd136
irpankhoiruljabar <77085160+irpankhoiruljabar@users.noreply.github.com>
2021-01-19 09:05:06 +0700

    hello.java

---
+++ hellow.java
@@ -0,0 +1,24 @@
+public static void main(String[] args) {
+      
+        
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 5;
+        
+        int jumlah = a + b + c + d;
+        
+        System.out.print("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d + ", total = ");
+        System.out.println(jumlah);
+       
+        System.out.println("a + c = " + a + c );
+        System.out.println("a + c = " + ( a + c) );
+    
+        System.out.println("b * d = " + (b * d) );
+
+        System.out.println("d / a = " + (d / a) );
+
+        
+    }   
+    
+}
